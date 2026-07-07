601b84eae490f89881395a8723f550d7399c0432
shubhamjha1234 <jha122023@gmail.com>
2026-07-07 17:03:07 +0530

    Express Basics

--- Server/index.js
+++ Server/index.js
@@ -1,36 +1,18 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+//const http = require("http");
+const express = require("express");
 
-const myserver = http.createServer((req,res)=>{
-    // console.log("Request has been made from browser");
-    if(req.url === "/favicon.ico") return res.end();
-   const log = `${Date.now()}: ${req.url} New Request Received\n`;
-   const myurl = url.parse(req.url,true);
-   console.log(myurl);
-   
-    fs.appendFile("log.txt", log , (err ,data) =>{
-    switch(myurl.pathname){
-        case "/": // /=>automatically goes to home page
-            res.end("Home Page");
-            break;
-            case"/about":
-              const username = myurl.query.myname;
-              res.end(`Hi,${username}`);
-                break;
-                case"/contact":
-                    res.end("Contact us");
-                    break;
-                    case"/search":
-                   const search = myurl.query.search_query;
-                   res.end(`Search results for ${search}`);
-                  break;
-
-                    case "/Default":
-                        res.end("Error 404 Page Not Found");
-                         };
-                        });
-     // console.log(req.headers);
+const app = express(); //app is handler function internally
+ 
+app.get("/", (req, res) => {
+    res.send("Hello From Home Page");
 });
 
-    myserver.listen(8000,()=> console.log("Listening on port 8000..."));
+app.get("/about", (req, res) => {
+    res.send("Hello From About Page"+ "Hey" + req.query.name+ "Your Age is "+ req.query.age);
+});
+
+
+
+//const myserver = http.createServer(myHandler);
+
+    app.listen(8000,()=> console.log("Server Started!"));
